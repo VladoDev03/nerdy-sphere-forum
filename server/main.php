f4f1ea3cee15fa3815259b7303ca50a9091b3119
vladodev03 <vladsto101@gmail.com>
2024-12-31 00:37:23 +0200

    Now connection to database uses constants.

--- server/main.php
+++ server/main.php
@@ -1,9 +1,13 @@
 <?php
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = 'T0rta@s@marmaladi7';
+const DB_NAME = 'feedback_tutorial';
 
-$connection = new mysqli("localhost", "root", "T0rta@s@marmaladi7", "nerdy_sphere_forum");
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
+    die('Connection failed: ' . $connection->connect_error);
 }
 
-echo "Connected successfully";
+echo 'Connected successfully';
