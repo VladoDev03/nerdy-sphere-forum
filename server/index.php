1cddd0d92ad9e6d7be209ab4ab6734c0dc458991
vladodev03 <vladsto101@gmail.com>
2025-01-17 02:28:04 +0200

    Made comments and replies hideable and visible.

--- server/index.php
+++ server/index.php
@@ -229,37 +229,39 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         </form>
         <div id="response"></div>
 
-<!--        --><?php //if (count($post['comments']) > 0): ?>
-            <div id="comment-section-<?= $post['id'] ?>" class="comment-section">
-                <?php foreach ($post['comments'] as $comment): ?>
-                    <div class="comment">
-                        <p class="comment-user"><?= $comment['username'] ?>:</p>
-                        <p class="comment-content"><?= $comment['content'] ?></p>
-                        <p class="comment-info">Posted at: <?= $comment['created_at'] ?></p>
+        <!--        --><?php //if (count($post['comments']) > 0): ?>
+        <details id="comment-section-<?= $post['id'] ?>" class="comment-section">
+            <summary>View Comments</summary>
+            <?php foreach ($post['comments'] as $comment): ?>
+                <div class="comment">
+                    <p class="comment-user"><?= $comment['username'] ?>:</p>
+                    <p class="comment-content"><?= $comment['content'] ?></p>
+                    <p class="comment-info">Posted at: <?= $comment['created_at'] ?></p>
 
-                        <form class="reply-form" action="index.php" method="POST">
+                    <form class="reply-form" action="index.php" method="POST">
                             <textarea class="reply-input" name="reply-input" id="reply-input-<?= $comment['id'] ?>"
                                       placeholder="Add a reply..."></textarea>
-                            <input type="hidden" id="post-id-<?= $comment['id'] ?>" value="<?= $post['id'] ?>">
-                            <button id="submit-reply-<?= $comment['id'] ?>" class="submit-reply">Send Reply</button>
-                        </form>
+                        <input type="hidden" id="post-id-<?= $comment['id'] ?>" value="<?= $post['id'] ?>">
+                        <button id="submit-reply-<?= $comment['id'] ?>" class="submit-reply">Send Reply</button>
+                    </form>
 
-<!--                        --><?php //if (count($comment['replies']) > 0): ?>
-                            <div id="reply-section-<?= $comment['id'] ?>">
-                                <?php foreach ($comment['replies'] as $reply): ?>
-                                    <div class="comment-reply">
-                                        <p class="comment-user"><?= $reply['username'] ?>:</p>
-                                        <p class="comment-content"><?= $reply['content'] ?></p>
-                                        <p class="comment-info">Posted at: <?= $reply['created_at'] ?></p>
-                                    </div>
-                                <?php endforeach; ?>
+                    <!--                        --><?php //if (count($comment['replies']) > 0): ?>
+                    <details id="reply-section-<?= $comment['id'] ?>">
+                        <summary>View Replies</summary>
+                        <?php foreach ($comment['replies'] as $reply): ?>
+                            <div class="comment-reply">
+                                <p class="comment-user"><?= $reply['username'] ?>:</p>
+                                <p class="comment-content"><?= $reply['content'] ?></p>
+                                <p class="comment-info">Posted at: <?= $reply['created_at'] ?></p>
                             </div>
-<!--                        --><?php //endif; ?>
-                    </div>
+                        <?php endforeach; ?>
+                    </details>
+                    <!--                        --><?php //endif; ?>
+                </div>
 
-                <?php endforeach; ?>
-            </div>
-<!--        --><?php //endif; ?>
+            <?php endforeach; ?>
+        </details>
+        <!--        --><?php //endif; ?>
     </div>
 <?php endforeach; ?>
 <script>
